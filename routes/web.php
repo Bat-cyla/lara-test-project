64b986c1261f044deb528f0a33f542944ba2bb05
Dmitriy <dmitrijnemov4@mail.ru>
2023-12-25 18:55:01 +0300

    second commit

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\FirstController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,5 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'AAAAAAAAAA';
 });
+
+Route::get('/my_page', [FirstController::class, 'index']);
